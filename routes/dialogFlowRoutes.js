d0e1a3b4234b6bdb9a8ec93d7516977d667f894a
sajad1993gh <sajad1993gh@gmail.com>
2019-09-14 01:35:25 -0400

    routes

---
+++ routes/dialogFlowRoutes.js
@@ -0,0 +1,19 @@
+module.exports = app => {
+    app.get('/', (req, res) => {
+        res.send({
+            'hello': 'Sajad'
+        })
+    });
+    
+    app.post('/api/df_text_query', (req, res) => {
+        res.send({
+            'do': 'text query'
+        })
+    });
+    
+    app.post('/api/df_event_query', (req, res) => {
+        res.send({
+            'do': 'event query'
+        })
+    });
+}
