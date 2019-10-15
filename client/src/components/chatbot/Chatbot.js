5c3aa3945c435d64de1c8c5a0312c71651c630af
sajad1993gh <sajad1993gh@gmail.com>
2019-10-14 23:48:11 -0400

    added render message props

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios/index';
+import Message from './Message';
 
 class Chatbot extends Component {
 
@@ -41,17 +42,27 @@ class Chatbot extends Component {
                 speaks: 'me',
                 msg: msg
             }
-            
+
             this.setState({messages: [...this.state.messages, says]})
 
         }
     }
 
+    renderMessages(stateMessages){
+        if(stateMessages){
+            return stateMessages.map((message, i) => {
+                return <Message key={i} speaks={message.speaks} text={message.msg.text.text} />
+            });
+        } else {
+            return null;
+        }
+    }
     render(){
         return (
             <div style={{height: 400, width: 400, float: 'right'}}>
                 <div id="chatbot" style={{height: '100%', width: '100%', overflow: 'auto'}}>
                     <h2>Chatbot</h2>
+                    {this.renderMessages(this.state.messages)}
                     <input type="text"/>
                 </div>
 
