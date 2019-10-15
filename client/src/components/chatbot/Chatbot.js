88b297256f8fa2014435c84f0adac0225d2cbbd6
sajad1993gh <sajad1993gh@gmail.com>
2019-10-14 23:12:42 -0400

    added stateful Chatbot component

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Chatbot = () => 
-    (
-        <div>
-            <h1>Chatbot will be here</h1>
-        
-        </div>
+class Chatbot extends Component {
 
-    )
+    constructor(props){
+        super(props);
+
+        this.state = {
+            messages: []
+        }
+    }
+
+    render(){
+        return (
+            <div style={{height: 400, width: 400, float: 'right'}}>
+                <div id="chatbot" style={{height: '100%', width: '100%', overflow: 'auto'}}>
+                    <h2>Chatbot</h2>
+                    <input type="text"/>
+                </div>
+
+            </div>
+        )
+    }
+}
+
 
 
 export default Chatbot;
