ae10497d5ce39af3a4e0aaf64241fbf0b6025659
sajad1993gh <sajad1993gh@gmail.com>
2019-10-14 23:37:05 -0400

    message component

---
+++ client/src/components/chatbot/Message.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Message = (props) => (
+    <div className="col s12 m8 offset-m2 offset-l3">
+        <div className="card-panel grey lighten-5 z-depth-1">
+            <div className="row valign-wrapper">
+                
+            </div>
+        </div>
+    </div>
+)
